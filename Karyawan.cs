7f9a6d564a20f13aab72e9c311abb92de74c2b91
djorgibimautama <djorgibima@gmail.com>
2021-04-13 14:44:37 +0700

    ResponsiPemrograman3376

---
+++ Karyawan.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemrograman3376
+{
+    class Karyawan
+    {
+        int NIK { get; set; }
+        String Nama { get; set; }
+        int GajiBulanan { get; set; }
+
+        public Karyawan(int NIK, string Nama, int GajiBulanan)
+        {
+            if (GajiBulanan < 0)
+            {
+                this.GajiBulanan = 0;
+            }
+            else
+            {
+                this.GajiBulanan = GajiBulanan;
+            }
+
+            this.NIK = NIK;
+            this.Nama = Nama;
+        }
+
+        public static void NaikGaji(Karyawan obj1, Karyawan obj2)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Kenaikan gaji 10%!");
+            Console.WriteLine(" ");
+            obj1.GajiBulanan += obj1.GajiBulanan * 10 / 100;
+            obj2.GajiBulanan += obj2.GajiBulanan * 10 / 100;
+
+        }
+        public static void Show(Karyawan obj1, Karyawan obj2)
+        {
+            Console.WriteLine("No Nik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("1  {0} {1}\t{2}", obj1.NIK, obj1.Nama, obj1.GajiBulanan);
+            Console.WriteLine("2  {0} {1}\t{2}", obj2.NIK, obj2.Nama, obj2.GajiBulanan);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ResponsiPemrograman3376
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan kar1 = new Karyawan(201122333, "Paijo", 3500000);
+            Karyawan kar2 = new Karyawan(201122333, "Jono", 2500000);
+
+            Karyawan.Show(kar1, kar2);
+            Karyawan.NaikGaji(kar1, kar2);
+            Karyawan.Show(kar1, kar2);
+        }
+    }
+}
